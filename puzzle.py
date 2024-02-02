761098ab0424dc5de947b56e9c515c2b6fcca7fd
David <davvan@pdx.edu>
2024-02-02 14:17:13 -0800

    solved basic state, need to test more states

--- puzzle.py
+++ puzzle.py
@@ -1,99 +1,168 @@
 import heapq
 import copy
+
 WIDTH = 3
 HEIGHT = 3
 
-class Node():
-    def __init__(self, state, parent, action):
+GOAL_STATE = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "b"]]
+
+class Node:
+    def __init__(self, state, parent, h_value):
         self.state = state
+        self.h_value = h_value
         self.parent = parent
-        self.action = action
 
-class BestFirst():
-    def __init__(self):
-        self.frontier = []
-    
-    def add(self, node, h_value):
-        heapq.heappush(self.frontier, (h_value, node))
-    
-    def empty(self):
-        return len(self.frontier) == 0
-    
-    def remove(self):
-        node = heapq.heappop(self.frontier)
-        return node
+    def __lt__(self, other):
+        return self.heuristic_value() < other.heuristic_value()
 
-
-class Board():
-    def __init__(self, world):
-        self.world = world
-        self.goal_state = [[-1, 1, 2], [3, 4, 5], [6, 7, 8]]
-        self.h_value = 0
-        self.pos = self.get_position()
-    
-    def calc_huristic(self):
-        for i in range(WIDTH):
-            for j in range(HEIGHT):
-                if self.world[i][j] != self.goal_state[i][j]:
-                    if self.world[i][j] != -1:
-                        self.h_value += 1
-    
-    def get_position(self):
-        for i in range(WIDTH):
-            for j in range(HEIGHT):
-                if self.world[i][j] == -1:
-                    return (i,j)
-
-    #given a state, return all possible actions
-    def valid_actions(self, state):
-        i, j = self.pos
-        valid_move = [
-            ("up", self.swap((i -1, j))), 
-            ("down", self.swap((i + 1, j))),
-            ("right", self.swap((i, j+1))),
-            ("left", self.swap((i, j-1)))
-        ]
-        result = []
-        for move,state in valid_move:
-            if state != None:
-                result.append((move, state))
-        return result
-
-        
-    def swap(self, to_move):
-        i, j = to_move
-        bi, bj = self.pos
-        if 0 <= i <WIDTH and 0 <= j and j<HEIGHT:
-            new_state = copy.deepcopy(self.world)
-            to_move_value = new_state[i][j]
-            new_state[i][j] = -1
-            new_state[bi][bj] = to_move_value
-            return new_state
-        else:
-            return None
-        
-
-
-
-
-    
     def heuristic_value(self):
         return self.h_value
 
-    def show(self):
-        print()
-        for row in self.world:
-            print(row)
-        print()
+
+class BestFirst:
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node, h_value):
+        heapq.heappush(self.frontier, (h_value, node))
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        node = heapq.heappop(self.frontier)
+        return node[1]
+
+
+class NotReachable(Exception):
+    pass
+
+def calc_huristic(world):
+        h_value = 0
+        for i in range(WIDTH):
+            for j in range(HEIGHT):
+                if world[i][j] != GOAL_STATE[i][j]:
+                    if world[i][j] != "b":
+                        h_value += 1
+        return h_value
+def find_b(state):
+    for i in range(WIDTH):
+        for j in range(HEIGHT):
+            if state[i][j] == 'b':
+                return (i, j)
+
+# given a state, return all possible actions
+def valid_actions(state):
+    i, j= find_b(state)
+    valid_move = [
+        swap((i,j), (i - 1, j), state),
+        swap((i,j), (i + 1, j), state),
+        swap((i,j), (i, j + 1), state),
+        swap((i,j), (i, j - 1), state),
+    ]
+    result = []
+    for state in valid_move:
+        if state != None:
+            result.append((state))
+    return result
+
+def swap(bpos, to_move, world):
+    i, j = to_move
+    bi, bj = bpos
+    if 0 <= i < WIDTH and 0 <= j and j < HEIGHT:
+        new_state = copy.deepcopy(world)
+        to_move_value = new_state[i][j]
+        new_state[i][j] = "b"
+        new_state[bi][bj] = to_move_value
+        return new_state
+    else:
+        return None
+
+class Board:
+    def __init__(self, world):
+        self.world_list = self.make_list(world)
+        self.world = [
+            self.world_list[i : i + 3] for i in range(0, len(self.world_list), 3)
+        ]
+        if self.count_inversions() % 2 != 0:
+            raise NotReachable("Cannot reach goal state")
+        self.h_value = 0
+        self.pos = self.get_position()
+        self.cost = 0
+
+    def make_list(self, world):
+        result = [
+            char if char.isdigit() else "b" if char == "b" else char
+            for char in world.split()
+        ]
+        return result
+
+    def count_inversions(self):
+        inversions = 0
+        current = 0
+        n = WIDTH * HEIGHT
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                current = self.world_list[i]
+                index = self.world_list[j]
+                if current != "b" and index != "b":
+                    if int(current) > int(index):
+                        inversions += 1
+
+        return inversions
+
+    def get_position(self):
+        for i in range(WIDTH):
+            for j in range(HEIGHT):
+                if self.world[i][j] == "b":
+                    return (i, j)
 
     
 
+    def start(self):
+        huristic_state = calc_huristic(self.world)
+        if huristic_state == 0:
+            return
+        start = Node(self.world, None, huristic_state)
+        frontier = BestFirst()
+
+        frontier.add(start, huristic_state)
+
+        self.explored = set()
+
+        while True:
+            node = frontier.remove()
+            actions = valid_actions(node.state)
+
+            if node.state == GOAL_STATE:
+                print("Done")
+                show(node.state)
+                print(self.cost)
+                return
+            self.cost += 1
+
+            state = tuple(tuple(row) for row in node.state)
+            self.explored.add(state)
+
+            for state in actions:
+                huristic_state = calc_huristic(state)
+                state_hash = tuple(tuple(row) for row in state)
+                if state_hash not in self.explored:
+                    child = Node(state, node, huristic_state)
+                    frontier.add(child, huristic_state)
 
 
+def show(world):
+    print()
+    for row in world:
+        print(row)
+    print()
 
 
 def main():
-    world = [[-1, 2, 3], [1, 4, 5], [8, 7, 6]]
+    world = "b 1 3 4 2 5 7 8 6"
+    b = Board(world)
+    b.start()
 
 
 if __name__ == "__main__":
--- test_puzzle.py
+++ test_puzzle.py
@@ -1,28 +1,77 @@
-from puzzle import Board
+import pytest
+from puzzle import Board, NotReachable, calc_huristic
 
-world = [
-        [7,2,4],
-        [5,-1,6],
-        [8,3,1]
-   ] 
+world = ("7 2 4 5 b 6 8 3 1")
 b = Board(world)
 
 def test_huristic():
-   b.calc_huristic()
-   assert b.heuristic_value() == 8
 
-def test_enumerating_action():
-   up_state = [
-        [7,-1,4],
-        [5,2,6],
-        [8,3,1]
-   ] 
-   actions_states = b.valid_actions(world)
-   actions = [action[0] for action in actions_states]
-   states = [state[1] for state in actions_states]
+   partial_correct = [
+       ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['b', '7', '8']
+   ]
+   assert calc_huristic(partial_correct) == 2
 
-   assert "up" in actions
-   assert up_state in states
+   partial_correct = [
+      ['b', '1', '3'],
+    ['4', '2', '5'],
+    ['7', '8', '6']
+   ]
+   assert calc_huristic(partial_correct) == 4
+
+   goal_state = [
+       ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', 'b']
+   ]
+   assert calc_huristic(goal_state) == 0
+
+def test_board_raise_notreachable():
+   inversion = "1 2 3 b 4 6 8 5 7"
+   with pytest.raises(NotReachable):
+      inversion_board = Board(inversion)
+   even_inversion = "b 1 3 4 2 5 7 8 6"
+   board_good = Board(even_inversion)
+   assert board_good.count_inversions() == 4
+   
+
+
+# def test_enumerating_action():
+#    up_state = [['7', 'b', '4'], ['5', '2', '6'], ['8', '3', '1']]
+#    states = b.valid_actions()
+#    assert up_state in states
+
+# def test_invalid_actions():
+#    right = [
+#       ['7', '2', '4'],
+#     ['5', '3', '6'],
+#     ['8', '1', 'b']
+#    ]
+#    left = [
+#       ['7', '2', '4'],
+#     ['5', '3', '6'],
+#     ['b', '8', '1']
+#    ]
+#    no_down = Board("7 2 4 5 3 6 8 b 1")
+#    states = no_down.valid_actions()
+#    assert len(states) == 3
+#    assert right in states
+#    assert left in states
+
+#    n_right = Board("2 7 4 5 3 b 8 6 1")
+#    states = n_right.valid_actions()
+#    assert len(states) == 3
+
+# def test_corner_actions():
+#    left_corner = [['2', 'b', '4'], ['5', '3', '7'], ['8', '1', '6']]
+#    n_right = Board("b 2 4 5 3 7 8 1 6")
+#    states = n_right.valid_actions()
+#    assert len(states) == 2
+#    assert left_corner in states
+
+
+
 
 if __name__ == "__main__":
-   test_enumerating_action()
+   test_huristic()
